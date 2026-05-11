b98d2a7c8a72bc084801cdb980f6bb8e295ae619
Алексей Шашин <alexshashin.2023@gmail.com>
2026-05-11 16:25:10 +0500

    Реализованное представление модели таймера. Шашин

---
+++ TimeLord_MVVM_Shashin/ViewModell/VMStopwatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Threading;
+using TimeLord_MVVM_Shashin.Modell;
+
+namespace TimeLord_MVVM_Shashin.ViewModell
+{
+    public class VMStopwatch : INotifyPropertyChanged
+    {
+        public Stopwatch Stopwatch { get; set; }
+        private DispatcherTimer Timer = new DispatcherTimer()
+        {
+            Interval = new System.TimeSpan(0, 0, 1)
+        };
+        public VMStopwatch()
+        {
+            Stopwatch = new Stopwatch()
+            {
+                Work = false,
+                Time = 0
+            };
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (Stopwatch.Work)
+                Stopwatch.Time++;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
